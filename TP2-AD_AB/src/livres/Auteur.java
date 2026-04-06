2cb158436c6c1ba48ca2b3cff7d957ae3b12a8b2
2354193 <2354193@cegeplimoilou.ca>
2026-04-06 18:34:38 -0400

    arranger tout

---
+++ TP2-AD_AB/src/livres/Auteur.java
@@ -0,0 +1,69 @@
+package livres;
+
+import java.util.Objects;
+
+public class Auteur {
+
+    public static final String INCONNU = "Inconnu";
+
+    private String prenom = INCONNU;
+    private String nom = INCONNU;
+    private Pays paysOrigine;
+
+    public Auteur(String prenom, String nom, Pays paysOrigine) {
+        setPrenom(prenom);
+        setNom(nom);
+        setPaysOrigine(paysOrigine);
+    }
+
+    public Auteur(String prenom, String nom, String pays) {
+        this.prenom = prenom;
+        this.nom = nom;
+
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    private void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    private void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Pays getPaysOrigine() {
+        return paysOrigine;
+    }
+
+    public void setPaysOrigine(Pays paysOrigine) {
+        this.paysOrigine = paysOrigine;
+    }
+
+    @Override
+    public String toString() {
+        return prenom + " " + nom + " (origine : " + paysOrigine + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Auteur)) return false;
+
+        Auteur auteur = (Auteur) o;
+        return prenom.equals(auteur.prenom)
+                && nom.equals(auteur.nom)
+                && Objects.equals(paysOrigine, auteur.paysOrigine);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(prenom, nom, paysOrigine);
+    }
+}
